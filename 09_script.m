a79dc93e3d56086d77d34f912424d9a20ea804f4
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-04-15 17:17:59 +0530

    Final

--- 09_script.m
+++ 09_script.m
@@ -9,15 +9,16 @@ AIM:Writing brief Scripts starting each Script with a request for input(using in
 SOFTWARE USED: MATLAB 9.12.0(R2022a)
   %}
 
-%The Script
-H=0
-T=input('Enter the value of T:')
-if(T==0)
-  disp('Enter a value greater than 0')
-else if(0<T && T<100)
-  fprintf('For T=%d',T)
-  H=((0.45*T)+900)
-  end
+% Prompt user for input
+T = input('Enter a value for T: ');
+
+% Evaluate h(T) using if-else statement
+if T > 0 && T < 100
+    h = T - 10;
+elseif T >= 100
+    h = 0.45*T + 900;
+else
+    h = NaN; % Not a number for invalid input
 end
-fprintf('H=%d',H)
-  
+fprintf('h(%d) = %f\n', T, h);
+
