183cc48f2e092bcc29ec3233d4f0c149df3ea592
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-04-21 04:24:18 +0530

    --Final--

--- open_ended.m
+++ open_ended.m
@@ -1,23 +1,27 @@
 %{ Aim: Given the data about Blood Pressure and Cholesterol for 20 patients, using k-means clustering to group the patients into having high risk of heart attack and those having low risk of heart attack.}%
 
-%Load the data into MATLAB as a matrix
-data = [120 70; 130 85; 125 75; 135 80; 140 90; 142 92; 130 75; 125 80; 130 85; 128 82; 130 78; 125 70; 135 80; 140 90; 130 75; 125 80; 130 85; 128 82; 130 78; 125 70];
-
-%Define the number of clusters you want to create
+% Load data from file
+data = [120 180; 130 190; 140 210; 120 170; 110 160; 130 200; 140 190; 130 180; 
+120 190; 130 210; 140 200; 120 180; 110 170; 130 190; 140 210; 130 170; 120 200; 
+130 190; 140 200; 120 180];
+% Set k value for k-means clustering
 k = 2;
-
-%Run k-means clustering on the data
-[idx, C] = kmeans(data, k);
-
-%Plot the results
+% Run k-means clustering algorithm
+[idx, centroids] = kmeans(data, k);
+% Print the clusters and their corresponding patients
+for i = 1:k
+ cluster_idx = find(idx == i);
+ fprintf('Cluster %d:\n', i);
+ fprintf('Patients: %s\n', num2str(cluster_idx'));
+ fprintf('Centroid: %s\n\n', num2str(centroids(i,:)));
+end
+% Plot the clusters and their corresponding patients
 figure;
-plot(data(idx==1,1),data(idx==1,2),'r.','MarkerSize',12)
-hold on
-plot(data(idx==2,1),data(idx==2,2),'b.','MarkerSize',12)
-plot(C(:,1),C(:,2),'kx',...
-     'MarkerSize',15,'LineWidth',3)
-legend('Cluster 1','Cluster 2','Centroids',...
-       'Location','NW')
-title 'Blood Pressure and Cholesterol Clustering';
-xlabel 'Blood Pressure';
-ylabel 'Cholesterol';
+scatter(data(idx==1,1), data(idx==1,2), 'r');
+hold on;
+scatter(data(idx==2,1), data(idx==2,2), 'b');
+scatter(centroids(:,1), centroids(:,2), 'k', 'filled');
+legend('Low Risk', 'High Risk', 'Centroids');
+xlabel('Blood Pressure');
+ylabel('Cholesterol');
+title('Blood Pressure and Cholesterol Clustering');
